1ad017d76c919cda54aa89b15ac4721862665d67
ColinKintzinger <157931650+ColinKintzinger@users.noreply.github.com>
2024-12-30 10:22:58 -0600

    update day1.py

--- 2024/Day1.py
+++ 2024/Day1.py
@@ -13,6 +13,20 @@ def get_file_data():
     file.close()
 def tranposing(col, row, cord):
     tr = [[0 for _ in range(row)] for _ in range(col)]
+
+    for i in range(row):
+        for j in range(col):
+            tr[j][i] = cord[i][j]
+    return tr
+def row_wise_sort(M):
+    for i in range(len(M)):
+        M[i] = sorted(M[i])
+    return M
+
+def sort_col(col, row, cord):
+
+    B= tranposing(col, row, cord)
+    
 # Print the 2D array
 for row in cord:
     print(row)
