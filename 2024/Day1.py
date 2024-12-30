5cab36774b694c9308bfb00830c2722ab224f99e
ColinKintzinger <157931650+ColinKintzinger@users.noreply.github.com>
2024-12-30 10:22:29 -0600

    continued work on day 1

--- 2024/Day1.py
+++ 2024/Day1.py
@@ -1,1 +1,19 @@
-file = open("input.txt", "r")
+file = open("2024\\input.txt", "r")
+cord = [[]]
+
+# Read each line from the file
+#lines = file.readlines()
+def get_file_data():
+    for line in file:
+        # Split the line into columns and convert to integers if necessary
+        
+        columns = line.strip().split()
+
+        cord.append(columns)
+    file.close()
+def tranposing(col, row, cord):
+    tr = [[0 for _ in range(row)] for _ in range(col)]
+# Print the 2D array
+for row in cord:
+    print(row)
+
