0f64924f29159a820db1e812146dc6527124a8b6
ColinKintzinger <majic11300@gmail.com>
2024-12-30 18:57:22 -0600

    update day1.py

--- 2024/Day1.py
+++ 2024/Day1.py
@@ -1,33 +1,13 @@
 file = open("2024\\input.txt", "r")
-cord = [[]]
+row = 1000
+col = 2
+cord = []
 
-# Read each line from the file
-#lines = file.readlines()
-def get_file_data():
-    for line in file:
-        # Split the line into columns and convert to integers if necessary
-        
-        columns = line.strip().split()
-
-        cord.append(columns)
-    file.close()
-def tranposing(col, row, cord):
-    tr = [[0 for _ in range(row)] for _ in range(col)]
-
-    for i in range(row):
-        for j in range(col):
-            tr[j][i] = cord[i][j]
-    return tr
-def row_wise_sort(M):
-    for i in range(len(M)):
-        M[i] = sorted(M[i])
-    return M
-
-def sort_col(col, row, cord):
-
-    B= tranposing(col, row, cord)
+for line in file:
+    line = list(map(int, line.strip().split()))
+    cord.append(line)
     
-# Print the 2D array
+cord.sort(key = lambda x: x[0])
+cord.sort(key= lambda x: x[1])
 for row in cord:
     print(row)
-
