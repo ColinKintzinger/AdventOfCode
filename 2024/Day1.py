f897da523d08ae266b8c751527c5156a1ccf5b51
ColinKintzinger <157931650+ColinKintzinger@users.noreply.github.com>
2025-01-04 11:22:24 -0600

    finished day 1
    completed the objective of day one part one and part 2

--- 2024/Day1.py
+++ 2024/Day1.py
@@ -2,12 +2,47 @@ file = open("2024\\input.txt", "r")
 row = 1000
 col = 2
 cord = []
+left = []
+right= []
+distance = 0
+sim = 0
+def is_negative(num):
+    if num < 0:
+        return True
+    return False
+def finding_distance(left, right, distance ):
+    tmp =0 
+    for i in range(row):
+
+        tmp = left[i] - right[i]
+        if is_negative(tmp):
+            tmp = tmp * -1
+            distance += tmp
+        else:
+            distance += tmp
+    return distance
+def simularity_rating(left, right, sim):
+    for i in range(row):
+        count =0
+        for j in range(row):
+            if left[i] == right[j]:
+                count += 1
+        sim += left[i] * count
+    return sim
 
 for line in file:
     line = list(map(int, line.strip().split()))
     cord.append(line)
-#cord.sort(cord[0])    
-cord.sort(key=lambda x: (x[0]. x[1]))
-#cord.sort(key= lambda x: x[1])
-for row in cord:
-    print(row)
+for i in range(row):
+    left.append(cord[i][0])
+    right.append(cord[i][1])
+left.sort()
+right.sort()
+for i in range(row):
+    cord[i][0] = left[i]
+    cord[i][1] = right[i]
+
+print ("Part 1:")
+print (finding_distance(left, right, distance))
+print ("Part 2:")
+print (simularity_rating(left, right, sim))
