7ecf84ad8a49a8746f88512c45fca18b8f9dfbdc
ColinKintzinger <157931650+ColinKintzinger@users.noreply.github.com>
2025-01-28 10:38:44 -0600

    update of day 2

--- 2024/Day2.py
+++ 2024/Day2.py
@@ -1,1 +1,9 @@
+
 file = open("2024\\input_day_2.txt", "r")
+cord= []
+def importing_file():
+    for line in file:
+        line = list(map(int, line.strip().split()))
+        cord.append(line)
+importing_file()
+print(cord)
