3fdd46ec59ede8c12e5df163182fd7471e0a465d
ColinKintzinger <157931650+ColinKintzinger@users.noreply.github.com>
2025-01-29 14:05:14 -0600

    update

--- 2024/Day2.py
+++ 2024/Day2.py
@@ -5,5 +5,9 @@ def importing_file():
     for line in file:
         line = list(map(int, line.strip().split()))
         cord.append(line)
+def check_list(cord):
+    for i in range(len(cord)):
+        for j in range(len(cord[i])-1):
+            pass
 importing_file()
 print(cord)
